e8b5adeae80ea5f7313338a0c55794d1e0914081
januszgo <62623687+januszgo@users.noreply.github.com>
2023-04-03 06:55:02 +0200

    login works correctly

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, inspect, Table, Column, Integer, String, Float, MetaData
+import os
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mssql+pyodbc://chmurowe:krkdbPch23!@chmurowedb.database.windows.net:1433/accountdb?driver=ODBC+Driver+17+for+SQL+Server"
 
+db = SQLAlchemy(app)
+eng = create_engine("mssql+pyodbc://chmurowe:krkdbPch23!@chmurowedb.database.windows.net:1433/accountdb?driver=ODBC+Driver+17+for+SQL+Server")
+insp = inspect(eng)
+accounts = insp.get_table_names()
+meta = MetaData()
 
-@app.route('/')
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    value = db.Column(db.Double)
+
+@app.route('/', methods=['GET'])
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    if request.args.get('name') is not None and not inspect(eng).has_table(request.args.get('name')):
+        nt = Table(
+        request.args.get('name'), meta, 
+        Column('id', Integer, primary_key=True), 
+        Column('name', String(255)), 
+        Column('value', Float),
+        )
+        meta.create_all(eng)
+        accounts.append(request.args.get('name'))
+        accounts.sort()
+    print('Request for index page received')
+    return render_template('index.html', accounts = accounts)
 
 @app.route('/favicon.ico')
 def favicon():
